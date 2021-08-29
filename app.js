50608c448ef3ad4613154eb8056a7aa7c3484eaa
PrakashKunal <87089252+PrakashKunal@users.noreply.github.com>
2021-08-29 18:38:03 +0530

    update-Added translation api and logic

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ var inputtxt=document.querySelector('#input');
 var translatebtn=document.querySelector('#btn-translate');
 var output=document.querySelector('#output')
 translatebtn.addEventListener('click',function(){
-    output.innerText=inputtxt.value;
+    translate(inputtxt.value);
 });
+async function translate(text)
+{
+    var config={ params: {text:text} }
+    var res= await axios.get("https://api.funtranslations.com/translate/minion",config);
+    output.innerText=res.data.contents.translated
+}
